084735b9c3799487ed8d65a5453adc3381a550e0
William Phoenix Paul <phoenixzero4@gmail.com>
2026-02-12 07:29:51 -0700

    Initial Commit

---
+++ data-demo/src/test/java/com/revature/data_demo/DataDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.data_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

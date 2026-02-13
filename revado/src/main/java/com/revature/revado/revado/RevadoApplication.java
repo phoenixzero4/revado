186a6e948b78a90fb28c06a486431330077c4106
William Phoenix Paul <phoenixzero4@gmail.com>
2026-02-13 13:17:28 -0700

    First commit

---
+++ revado/src/main/java/com/revature/revado/revado/RevadoApplication.java
@@ -0,0 +1,13 @@
+package com.revature.revado.revado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevadoApplication.class, args);
+	}
+
+}
---
+++ revado/src/test/java/com/revature/revado/revado/RevadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.revado.revado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
